8055cb8139a8c78bd7a7cd6af97eb52aa4f5c0b2
Ivanpisonero <91685630+Ivanpisonero@users.noreply.github.com>
2021-10-03 10:13:33 +0200

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,13 @@
+package es.ucm.tp1.logic;
+
+public class Player {
+	private int position;
+	
+	public Player() {
+		this.position = 0;
+	}
+	
+	public void positionUp() {
+		this.position++;
+	}
+}
