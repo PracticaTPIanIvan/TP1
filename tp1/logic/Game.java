2b35ca6e50eb5f54e266252e4a6d615747c14b71
Ian354 <91690959+Ian354@users.noreply.github.com>
2021-10-07 20:11:48 +0200

    Update Game.java

--- tp1/logic/Game.java
+++ tp1/logic/Game.java
@@ -6,14 +6,36 @@ public class Game {
 	
 	private Player player;
 	
+	private Level level;
+	
+	private Obstacle obstacle;
+	
+	private int [] obstacleList;
+	
+	private static final String PLAYER_ICON = ">";
+	
+	private static final String OBSTACLE_ICON = "░";
 
-	public Game(long seed, Level level) {
-		// TODO
+	public Game(long seed, Level level, int obstacleList) {
+		this.level = level;
 		this.player = new Player();
+		this.obstacleList = new int [2];
+	}
+	
+	public int getPlayerPositionX() {
+		return player.getPositionX();
+	}
+	
+	public int getPlayerPositionY() {
+		return player.getPositionY();
 	}
 	
 	public void moveUp() {
 		player.positionUp();	
+	}
+	
+	public void moveDown() {
+		player.positionDown();
 	}
 	
 	public void toggleTest() {
@@ -28,12 +50,24 @@ public class Game {
 		return 3;
 	}
 	
+	public double getObstacleFrequency() {
+		return level.getObstacleFrequency();
+	}
+	
+	public double getCoinFrequency() {
+		return level.getCoinFrequency();
+	}
+	
 	public String getGameStatus() {
 		return "";
 	}
 
 	public String positionToString(int j, int i) { 
-		return "";
+		if(j == player.getPositionX() && i == player.getPositionY()) {
+			return PLAYER_ICON;
+		}
+		else {
+			return "";
+		}
 	}
 }
-
