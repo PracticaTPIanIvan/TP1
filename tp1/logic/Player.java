5f3c40fc5c730b6f1d40f27ca69f1f0823c8b530
Ivanpisonero <91685630+Ivanpisonero@users.noreply.github.com>
2021-10-14 16:36:18 +0200

    Update Player.java

--- tp1/logic/Player.java
+++ tp1/logic/Player.java
@@ -3,10 +3,12 @@ package es.ucm.tp1.logic;
 public class Player {
 	private int positionX;
 	private int positionY;
+	private int numCoins;
 	
 	public Player() {
 		this.positionX = 0;
 		this.positionY = 1;
+		this.numCoins = 0;
 	}
 	
 	public int getPositionX() {
@@ -15,6 +17,14 @@ public class Player {
 	
 	public int getPositionY() {
 		return positionY;
+	}
+	
+	public int getCoins() {
+		return numCoins;
+	}
+	
+	public void advance() {
+		positionX += 1;
 	}
 	
 	public void positionUp() {
