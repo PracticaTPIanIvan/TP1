146d97b90df9905038a55fc18e7bbf9a4c351117
Ian354 <91690959+Ian354@users.noreply.github.com>
2021-11-07 11:17:58 +0100

    Update Coin.java

--- supercars/logic/gameobjects/Coin.java
+++ supercars/logic/gameobjects/Coin.java
@@ -3,12 +3,14 @@ package es.ucm.tp1.supercars.logic.gameobjects;
 import es.ucm.tp1.supercars.logic.Game;
 
 public class Coin extends GameObject {
-	public boolean alive;
+	
+	public static int counter;
 	
 	public Coin(Game game, int x, int y) {
 		super(game, x, y);
 		symbol = "¢";
 	}
+	
 	public void onEnter() {
 		counter++;
 	}
@@ -18,12 +20,22 @@ public class Coin extends GameObject {
 	}
 
 	public void onDelete() {
-		
+		alive = false;
+		counter--;
+	}
+	
+	public int getCounter() {
+		return counter;
 	}
 	
 	public boolean doCollision() {
 		return true;
 	}
+	
+	public static void reset() {
+		counter = 0;
+	}
+	
 	@Override
 	public boolean receiveCollision(Player player) {
 		player.addCoins();
