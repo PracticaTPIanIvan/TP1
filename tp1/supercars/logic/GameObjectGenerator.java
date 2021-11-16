ee5edb0085c747f83ab66caad89f11f4ee52c72a
Ian354 <91690959+Ian354@users.noreply.github.com>
2021-11-16 19:43:52 +0100

    Update GameObjectGenerator.java

--- tp1/supercars/logic/GameObjectGenerator.java
+++ tp1/supercars/logic/GameObjectGenerator.java
@@ -3,14 +3,25 @@ package es.ucm.tp1.supercars.logic;
 import es.ucm.tp1.supercars.control.Level;
 import es.ucm.tp1.supercars.logic.gameobjects.Coin;
 import es.ucm.tp1.supercars.logic.gameobjects.Obstacle;
+import es.ucm.tp1.supercars.logic.gameobjects.Wall;
+import es.ucm.tp1.supercars.logic.gameobjects.SuperCoin;
 
 public class GameObjectGenerator {
+	
+	private Level level;
 
 	public static void generateGameObjects(Game game, Level level) {
 
 		for (int x = game.getVisibility() / 2; x < game.getRoadLength(); x++) {
 			game.tryToAddObject(new Obstacle(game, x, game.getRandomLane()), level.getObstacleFrequency());
 			game.tryToAddObject(new Coin(game, x, game.getRandomLane()), level.getCoinFrequency());
+			
+			if(level.hasAdvancedObjects()) {
+				game.tryToAddObject(new Wall(game, x, game.getRandomLane()), level.advancedObjectsFrequency());
+				if(!SuperCoin.created) {
+					game.tryToAddObject(new SuperCoin(game, x, game.getRandomLane()), level.advancedObjectsFrequency());
+				}
+			}
 		}
 	}
 
