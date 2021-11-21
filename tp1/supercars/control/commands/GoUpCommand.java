b4719e34a50d1c929907b97fc8a9b5251fa5f82c
Ian354 <91690959+Ian354@users.noreply.github.com>
2021-11-21 12:13:50 +0100

    Update GoUpCommand.java

--- tp1/supercars/control/commands/GoUpCommand.java
+++ tp1/supercars/control/commands/GoUpCommand.java
@@ -1,39 +1,32 @@
 package es.ucm.tp1.supercars.control.commands;
 
 import es.ucm.tp1.supercars.logic.Game;
-import es.ucm.tp1.supercars.utils.StringUtils;
 
-public class HelpCommand extends Command {
+public class GoUpCommand extends Command {
+	
+	private static final String NAME = "up";
 
-	private static final String NAME = "help";
+	private static final String DETAILS = "[q]up";
 
-	private static final String DETAILS = "[h]elp";
+	private static final String SHORTCUT = "q";
 
-	private static final String SHORTCUT = "h";
-
-	private static final String HELP = "show this help";
-
-	public HelpCommand() {
+	private static final String HELP = "go up";
+	
+	protected static final String ERROR = "Player can't move up";
+			
+	public GoUpCommand() {
 		super(NAME, SHORTCUT, DETAILS, HELP);
 	}
-
-	@Override
+	
 	public boolean execute(Game game) {
-		StringBuilder buffer = new StringBuilder("Available commands:");
-
-		buffer.append("Available commands: \n" +
-		"[h]elp: show this help \n" +
-		"[i]nfo: prints gameobjet info \n" +
-		"[n]one | []: update \n" +
-		"[q]: go up \n" +
-		"[a]: go down \n" +
-		"[e]xit: exit game \n" +
-		"[r]eset: reset game \n" +
-		"[t]est: enables test mode");
-
-		System.out.println(buffer.toString());
-
-		return false;
+		if(game.getPlayerPositionY() > 0 && game.getPlayerPositionX() < game.getRoadLength()) {
+			game.moveUp();
+			game.update();
+			game.incrementCycle();
+			return true;
+		} else {
+			System.out.println(ERROR);
+			return false;
+		}
 	}
-
 }
